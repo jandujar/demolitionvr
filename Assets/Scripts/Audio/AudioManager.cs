0e9f13e2c48500c7905d5925c1be62ce0bab5b7e
Oscar <oscarvergaraj1@gmail.com>
2019-03-26 17:10:08 +0100

    Audio

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static void PlayAudio(AudioClip _clip, Transform _position){
+        GameObject tempObj = Instantiate(Resources.Load("AudioInstance") as GameObject, _position);
+        tempObj.GetComponent<AudioSource>().clip = _clip;
+        tempObj.GetComponent<AudioInstanceManager>().PlaySound();
+    }
+}
