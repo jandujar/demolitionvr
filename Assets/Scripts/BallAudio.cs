234b3dae5acbd42bf625746f041707ef9f44456a
jmaldonado_gamedesign <jmaldonado.gamedesign@gmail.com>
2019-03-26 17:44:03 +0100

    fuck

--- Assets/Scripts/BallAudio.cs
+++ Assets/Scripts/BallAudio.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class BallAudio : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+    [SerializeField] AudioClip[] ballAudios;
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "BuildingObject"){
+            AudioManager.Instance.PlayAudio(ballAudios[0], transform);
+        }
         
     }
 }
